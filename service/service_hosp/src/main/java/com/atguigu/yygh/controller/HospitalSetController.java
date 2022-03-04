ef919969e988fdda565c9bb8c0c7949f756a69dc
sakura <sakura@824.com>
2022-03-04 16:15:38 +0800

    crud for hospset 3/4

---
+++ service/service_hosp/src/main/java/com/atguigu/yygh/controller/HospitalSetController.java
@@ -0,0 +1,117 @@
+package com.atguigu.yygh.controller;
+
+import com.atguigu.yygh.common.result.Result;
+import com.atguigu.yygh.common.util.MD5;
+import com.atguigu.yygh.model.hosp.HospitalSet;
+import com.atguigu.yygh.service.HospitalSetService;
+import com.atguigu.yygh.vo.hosp.HospitalSetQueryVo;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.api.R;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.List;
+import java.util.Random;
+
+@Api(tags = "医院设置管理")
+@RestController
+@RequestMapping("/admin/hosp/hospitalSet" )
+public class HospitalSetController {
+
+    @Autowired
+    private HospitalSetService hospitalSetService;
+
+    //查询所有信息
+    @ApiOperation(value = "获取所有医院设置")
+    @GetMapping("findAll")
+    public Result findHospitalSet(){
+        List<HospitalSet> list = hospitalSetService.list();
+        return Result.ok(list);
+    }
+
+    //逻辑删除
+    @ApiOperation(value = "逻辑删除医院设置")
+    @DeleteMapping("{id}")
+    public Result removeHospset(@PathVariable Long id){
+        boolean flag = hospitalSetService.removeById(id);
+        if(flag){
+            return Result.ok();
+        }else {
+            return Result.fail();
+        }
+    }
+
+    //条件查询带分页
+    @ApiOperation(value = "条件查询")
+    @GetMapping("findPage/{cuurent}/{limit}")
+    public Result findPage(@PathVariable long current,
+                           @PathVariable long limit,
+                           @RequestBody(required = false) HospitalSetQueryVo hospitalSetQueryVo){
+        //创建page对象，当前页 每页记录数
+        Page<HospitalSet> page = new Page<>(current,limit);
+        //构建条件
+        QueryWrapper<HospitalSet> wrapper = new QueryWrapper<>();
+        String hosname = hospitalSetQueryVo.getHosname();
+        String hoscode = hospitalSetQueryVo.getHoscode();
+        if(!StringUtils.isEmpty(hosname)){
+            wrapper.like("hosname",hospitalSetQueryVo.getHosname());
+        }
+        if (!StringUtils.isEmpty(hoscode)){
+            wrapper.eq("hoscode",hospitalSetQueryVo.getHoscode());
+
+        }
+        Page<HospitalSet> pageHosp = hospitalSetService.page(page, wrapper);
+        return Result.ok(pageHosp);
+    }
+
+    //添加医院设置
+    @ApiOperation(value = "添加")
+    @PostMapping("save")
+    public Result saveHospitalSet(@RequestBody HospitalSet hospitalSet){
+        //设置状态1使用
+        hospitalSet.setStatus(1);
+        //签名秘钥 随机数+时间经过MD5加密
+        Random random = new Random();
+        hospitalSet.setSignKey(MD5.encrypt(System.currentTimeMillis()+""+ random.nextInt(1000)));
+        boolean save = hospitalSetService.save(hospitalSet);
+        if(save){
+            return Result.ok();
+        }else {
+            return Result.fail();
+        }
+
+    }
+
+    //根据id获取医院
+    @ApiOperation(value = "id获取医院")
+    @GetMapping("getHospSet/{id}")
+    public Result getHospSet(@PathVariable Long id){
+        HospitalSet hospitalSet = hospitalSetService.getById(id);
+        return Result.ok(hospitalSet);
+    }
+
+    //修改医院设置
+    @ApiOperation(value = "修改")
+    @PostMapping("updateHospSet")
+    public Result updateHospitalSet(@RequestBody HospitalSet hospitalSet){
+         boolean flag = hospitalSetService.updateById(hospitalSet);
+         if(flag){
+             return Result.ok();
+         }else{
+             return Result.fail();
+         }
+    }
+
+    //批量删除医院设置
+    @ApiOperation(value = "批量删除")
+    @DeleteMapping("batchRemove")
+    public Result batchRemove(@RequestBody List<String> idList){
+        hospitalSetService.removeByIds(idList);
+        return Result.ok();
+    }
+}
---
+++ service/service_hosp/src/main/java/com/atguigu/yygh/mapper/HospSetMapper.java
@@ -0,0 +1,8 @@
+package com.atguigu.yygh.mapper;
+
+import com.atguigu.yygh.model.hosp.HospitalSet;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+public interface HospSetMapper extends BaseMapper<HospitalSet> {
+
+}
